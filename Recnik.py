f97bb28dbe9cf90fba01d2706ba1636f65e6237f
unknown <ana11011978@gmail.com>
2022-05-26 22:38:58 +0200

    Samo pocetni deo,nedovrseno

---
+++ Recnik.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import ImageTk, Image
+#from PyDictionary import PyDictionary
+#from googletrans import Translator
+
+root = tk.Tk()
+root.title('Dictionary')
+root.geometry('600x300')
+root['bg'] = 'white'
+frame = Frame(root,width=200,height=300,borderwidth=1,relief=RIDGE) 
+frame.grid(sticky="W") 
+
+
+
+
+root.mainloop()
